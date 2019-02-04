13e4db798e63bdb9ae5241aaa947b8728bdbbd46
Ed Sowell <jagman653@gmail.com>
2019-02-04 14:05:58 -0800

    changed bad work msg

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -268,7 +268,7 @@ public class Anagrams extends JFrame {
             feedbackLabel.setText("Correct! Try a new word!");
             getRootPane().setDefaultButton(nextTrial);
         } else {
-            feedbackLabel.setText("Wrong, you dummy! Try again!");
+            feedbackLabel.setText("Sucker! Try again!");
             guessedWord.setText("");
         }
 
