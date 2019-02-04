abf3b2d18d5ffb36bd1c174b5237cc994f362e0f
Ed Sowell <jagman653@gmail.com>
2019-02-04 14:22:05 -0800

    changed bad word msg to "You never learn, do you?"

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -268,7 +268,7 @@ public class Anagrams extends JFrame {
             feedbackLabel.setText("Correct! Try a new word!");
             getRootPane().setDefaultButton(nextTrial);
         } else {
-            feedbackLabel.setText("Try again, sucker!");
+            feedbackLabel.setText("You never learn, do you?");
             guessedWord.setText("");
         }
 
